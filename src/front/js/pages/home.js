08a9052b22860b44c9e61f8fce1ef3d5918927c6
Roy Mora Mora <rmoramo1@gmail.com>
2021-04-28 22:06:46 +0000

    landing page ajuste de index(fontawesome), creacion de clases css

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,24 +1,153 @@
-import React, { useContext } from "react";
+import React, { useContext, useDebugValue } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
+import { Link } from "react-router-dom";
 import "../../styles/home.scss";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">{store.message || "Loading message from the backend..."}</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-					Read documentation
-				</a>
-			</p>
+		<div className="container-fluid p-0">
+			<div className="row imgPrincipal p-0 m-0">
+				<div className="col-6" />
+				<div className="col-6 text-white text-center pt-5">
+					<h2 data-aos="zoom-in" className="pt-5">
+						Pulxe hace tu vida más fácil, conectándote con decenas de servicios pensados a tu medida. Sin
+						salir de casa, te conectamos con asistentes especializados en: tareas del hogar, mascotas,
+						mandados, mecánica, entrenamiento personal y belleza.
+					</h2>
+				</div>
+			</div>
+			<div className="col-12 text-center mb-5">
+				<div className="col-12 text-center mt-5">
+					<h1 data-aos="zoom-in">Como Funciona</h1>
+				</div>
+				<div className="row my-5">
+					<div className="col-3" data-aos="flip-left" data-aos-delay="50">
+						<div className="col-12">
+							<div className="countRounded mb-5">1</div>
+						</div>
+						<div className="col-12">
+							<h3>Cree una Cuenta</h3>
+						</div>
+					</div>
+					<div className="col-3" data-aos="flip-left" data-aos-delay="50">
+						<div className="col-12">
+							<div className="countRounded mb-5">2</div>
+						</div>
+						<div className="col-12">
+							<h3>Busque un Pulxe</h3>
+						</div>
+					</div>
+					<div className="col-3" data-aos="flip-left" data-aos-delay="50">
+						<div className="col-12">
+							<div className="countRounded mb-5">3</div>
+						</div>
+						<div className="col-12">
+							<h3>Solicitar Pulxe</h3>
+						</div>
+					</div>
+					<div className="col-3" data-aos="flip-left" data-aos-delay="50">
+						<div className="col-12">
+							<div className="countRounded mb-5">4</div>
+						</div>
+						<div className="col-12">
+							<h3>Calificar</h3>
+						</div>
+					</div>
+				</div>
+				<div className="col-12 text-center">
+					<Link to="/" className="btn btn-danger" data-aos="zoom-in">
+						Crear Mi Cuenta
+					</Link>
+				</div>
+			</div>
+			<div className="row p-0 m-0 text-center">
+				<div className="col-6 float-left greyMedium">
+					<p data-aos="zoom-in">Que es un PULXE</p>
+				</div>
+				<div className="col-6 float-left greyLight">
+					<p data-aos="fade-up">dkfjdkljfgkldsjfkljsdkljf</p>
+				</div>
+			</div>
+			<div className="bluePulxeBG row p-0 m-0 py-5 text-white text-center">
+				<div className="col-12 mb-3">
+					<h1 className="text3r" data-aos="zoom-in">
+						Que puedes encontar
+					</h1>
+				</div>
+				<div className="row w-100 my-3">
+					<div className="col-4" data-aos="flip-left" data-aos-delay="100">
+						<div className="col-12 text-center mb-3">
+							<i className="fas fa-home" />
+						</div>
+						<div className="col-12 mb-3">
+							<h3>Hogar</h3>
+						</div>
+						<div className="col-12">jkldjfkldjkldjkldjkljdkljsdl</div>
+					</div>
+					<div className="col-4" data-aos="flip-left" data-aos-delay="100">
+						<div className="col-12 text-center mb-3">
+							<i className="fas fa-dog" />
+						</div>
+						<div className="col-12 mb-3">
+							<h3>Mascotas</h3>
+						</div>
+						<div className="col-12">jkldjfkldjkldjkldjkljdkljsdl</div>
+					</div>
+					<div className="col-4" data-aos="flip-left" data-aos-delay="100">
+						<div className="col-12 text-center mb-3">
+							<i className="fas fa-motorcycle" />
+						</div>
+						<div className="col-12 mb-3">
+							<h3>Mandados</h3>
+						</div>
+						<div className="col-12">jkldjfkldjkldjkldjkljdkljsdl</div>
+					</div>
+				</div>
+				<div className="row w-100 my-3">
+					<div className="col-4" data-aos="flip-left" data-aos-delay="100">
+						<div className="col-12 text-center mb-3">
+							<i className="fas fa-cogs" />
+						</div>
+						<div className="col-12 mb-3">
+							<h3>Mecanica</h3>
+						</div>
+						<div className="col-12">jkldjfkldjkldjkldjkljdkljsdl</div>
+					</div>
+					<div className="col-4" data-aos="flip-left" data-aos-delay="100">
+						<div className="col-12 text-center mb-3">
+							<i className="fas fa-running" />
+						</div>
+						<div className="col-12 mb-3">
+							<h3>Entrenador Personal</h3>
+						</div>
+						<div className="col-12">jkldjfkldjkldjkldjkljdkljsdl</div>
+					</div>
+					<div className="col-4" data-aos="flip-left" data-aos-delay="100">
+						<div className="col-12 text-center mb-3">
+							<i className="fas fa-spa" />
+						</div>
+						<div className="col-12 mb-3">
+							<h3>Belleza</h3>
+						</div>
+						<div className="col-12">jkldjfkldjkldjkldjkljdkljsdl</div>
+					</div>
+				</div>
+			</div>
+			<div className="row p-0 m-0 text-center">
+				<div className="col-6 float-left greyMedium">
+					<p data-aos="zoom-in">Como ser un PULXE</p>
+				</div>
+				<div className="col-6 float-left greyLight">
+					<p data-aos="fade-up">dkfjdkljfgkldsjfkljsdkljf</p>
+					<div className="col-12">
+						<Link to="/" className="btn btn-danger" data-aos="zoom-in">
+							Publicar mi servicio
+						</Link>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
